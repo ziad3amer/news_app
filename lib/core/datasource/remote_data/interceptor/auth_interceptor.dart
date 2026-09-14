88e0cf7a8ffab89571d9ed3044ee04ec73b162c7
ZIAD AMER <yarb8418@gmail.com>
2026-09-14 12:34:39 +0300

    Refactor `AuthInterceptor` to handle expired sessions and improve code formatting
    - Implement a session expiration notification by showing a "Session Expired" `SnackBar` when a 401 error occurs.
    - Update navigation logic in `onError` to clear preferences and redirect the user to the `LoginScreen`.
    - Apply consistent formatting to the `token` retrieval logic and navigation parameters.

--- lib/core/datasource/remote_data/interceptor/auth_interceptor.dart
+++ lib/core/datasource/remote_data/interceptor/auth_interceptor.dart
@@ -10,9 +10,7 @@ import '../../local_data/user_repository.dart';
 class AuthInterceptor extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    final token = UserRepository()
-        .getUser()
-        ?.token;
+    final token = UserRepository().getUser()?.token;
 
     if (token != null) {
       options.headers["Authorization"] = "Bearer $token";
@@ -30,12 +28,16 @@ class AuthInterceptor extends Interceptor {
       PreferencesMangar().clear();
       Navigator.of(context).pushNamedAndRemoveUntil(
         MaterialPageRoute(
-            builder: (BuildContext context) {
-          return LoginScreen();
-        }
-        ) as String,
-            (route) => false,
+              builder: (BuildContext context) {
+                return LoginScreen();
+              },
+            )
+            as String,
+        (route) => false,
       );
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("Session Expired")));
     }
     handler.next(err);
   }
